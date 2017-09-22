88cecde496802c56374ccd6d3d402e46f4baf611
sozboke <sozboke@etu.edu.tr>
2017-09-22 22:38:28 +0300

    Adding text about automatic2 deployment

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -12,12 +12,12 @@ import spark.template.mustache.MustacheTemplateEngine;
 
 public class App
 {
-    public static boolean search(ArrayList<Integer> array, int e) {
+    public static boolean search(ArrayList<String> array, String e,String d) {
       System.out.println("inside search");
       if (array == null) return false;
 
-      for (int elt : array) {
-        if (elt == e) return true;
+      for (String elt : array) {
+        if (elt.equals(e)) return true;
       }
       return false;
     }
@@ -34,27 +34,27 @@ public class App
           String input1 = req.queryParams("input1");
           java.util.Scanner sc1 = new java.util.Scanner(input1);
           sc1.useDelimiter("[;\r\n]+");
-          java.util.ArrayList<Integer> inputList = new java.util.ArrayList<>();
+          java.util.ArrayList<String> inputList = new java.util.ArrayList<>();
           while (sc1.hasNext())
           {
-            int value = Integer.parseInt(sc1.next().replaceAll("\\s",""));
+            String value = sc1.next().replaceAll("\\s","");
             inputList.add(value);
           }
           System.out.println(inputList);
 
 
           String input2 = req.queryParams("input2").replaceAll("\\s","");
-          int input2AsInt = Integer.parseInt(input2);
+          String input2AsInt = input2;
 	  String input3 = req.queryParams("input3").replaceAll("\\s","");
-	  int input3AsInt = Integer.parseInt(input3);
+	  String input3AsInt = input3;
 
-	  boolean result1 = App.search(inputList, input2AsInt);
-          boolean result = App.search(inputList, input3AsInt);
+	  boolean result = App.search(inputList, input2AsInt);
+          boolean result1 = App.search(inputList, input3AsInt);
 	  
 
          Map map = new HashMap();
-          map.put("result1", result1);
-	  map.put("result", result);
+          map.put("result", result);
+	  map.put("result1", result1);
           return new ModelAndView(map, "compute.mustache");
         }, new MustacheTemplateEngine());
 
